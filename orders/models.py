f4c44564830361c0353413b759574777ff65a8aa
Benaiah Galavu <bentheaya@gmail.com>
2025-06-24 16:49:04 +0300

    orders models fix

--- orders/models.py
+++ orders/models.py
@@ -10,7 +10,7 @@ class Order(models.Model):
     )
 
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='in_progress')
     created_at = models.DateTimeField(auto_now_add=True)
 
