deaa55c3c7f9c2d1668a9efe83d49be91d927b36
Benaiah Galavu <bentheaya@gmail.com>
2025-06-24 12:09:01 +0300

    made register api public

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from .serializers import UserSerializer, RegisterSerializer
 from .models import User
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny] 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
