b74b3bd392f7df1249f1cb7071efa4ba56a13f94
Benaiah Galavu <bentheaya@gmail.com>
2025-06-26 09:39:55 +0300

    Phase 5 compelete

--- campus_delivery/urls.py
+++ campus_delivery/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
     path('api/', include('payment.urls')),
+    path('api/', include('delivery.urls')),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
